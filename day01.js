6e379c9a3875773305619d2f3459d75dabc25808
anne gardes <anne.gardes@hotmail.com>
2021-12-01 15:19:40 +0100

    day01

--- day01.js
+++ day01.js
@@ -3,7 +3,7 @@ const read = fs.readFileSync("input.txt");
 let data = read.toString().split("\n").map(Number);
 
 let partOne = data.filter((el,i,arr)=>el<arr[i+1]).length
-let partTwo = data.map((el,i,arr)=>el+arr[i+1]+arr[i+2]).filter(el=>!isNaN(el)).filter((el,i,arr)=>el<arr[i+1]).length
+let partTwo = data.map((el,i,arr)=>el+arr[i+1]+arr[i+2]).filter((el,i,arr)=>!isNaN(el) && el<arr[i+1]).length
 
 console.log(`Part one = ${partOne}
 Part two = ${partTwo}`)
